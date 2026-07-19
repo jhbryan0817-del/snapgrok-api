57a3da6cf2c2259cd34ac5fd17c37b986ca95da5
Baik Seungmin <seungminbaik7@gmail.com>
2026-07-20 01:30:57 +0800

    Add files via upload

---
+++ SnapGrok-v4.1/auth-config.js
@@ -0,0 +1,10 @@
+(() => {
+  "use strict";
+  globalThis.SnapGrokAuthConfig = Object.freeze({
+  "publishableKey": "pk_test_aW1tb3J0YWwtdHJvbGwtNDIuY2xlcmsuYWNjb3VudHMuZGV2JA",
+  "frontendApiUrl": "https://immortal-troll-42.clerk.accounts.dev",
+  "websiteUrl": "http://localhost:3000",
+  "syncHost": "http://localhost",
+  "serverUrl": "https://snapgrok-api.onrender.com"
+});
+})();
