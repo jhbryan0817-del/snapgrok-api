2bf7313c9ce8256e52e8c36529dd1bce567b30b3
jhbryan0817-del <jhbryan0817@gmail.com>
2026-07-29 16:00:40 +0800

    Add files via upload

---
+++ extension/auth-config.js
@@ -0,0 +1,10 @@
+(() => {
+  "use strict";
+  globalThis.SnapGrokAuthConfig = Object.freeze({
+  "publishableKey": "pk_live_Y2xlcmsuc25lYWtzb2x2ZS5jb20k",
+  "frontendApiUrl": "https://clerk.sneaksolve.com",
+  "websiteUrl": "https://www.sneaksolve.com",
+  "syncHost": "https://clerk.sneaksolve.com",
+  "serverUrl": "https://snapgrok-api.onrender.com"
+});
+})();
