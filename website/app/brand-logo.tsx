7625ed9ce473a76e29031aef7adb9a26bd43256c
jhbryan0817-del <jhbryan0817@gmail.com>
2026-08-16 02:45:33 +0800

    Delete website directory

--- website/app/brand-logo.tsx
+++
@@ -1,7 +0,0 @@
-export function BrandLogo({ compact = false }: { compact?: boolean }) {
-  return (
-    <span className={`brand-logo${compact ? " brand-logo-compact" : ""}`} aria-hidden="true">
-      <img className="brand-logo-image" src="/zenaian-logo-user.png" alt="" />
-    </span>
-  );
-}
