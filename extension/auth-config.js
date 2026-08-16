a4b37c943e3b804c63961d1758b368598a2a3a46
jhbryan0817-del <jhbryan0817@gmail.com>
2026-08-16 22:17:54 +0800

    Add files via upload

---
+++ extension/auth-config.js
@@ -0,0 +1,7 @@
+(() => {
+  "use strict";
+  globalThis.SnapGrokAuthConfig = Object.freeze({
+  "websiteUrl": "https://www.zenaian.com",
+  "serverUrl": "https://snapgrok-api.onrender.com"
+});
+})();
