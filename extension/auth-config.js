1cfba84bcbed2cde84cad1cd6364302c4a8a889f
jhbryan0817-del <jhbryan0817@gmail.com>
2026-08-16 22:16:25 +0800

    Delete extension directory

--- extension/auth-config.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  "use strict";
-  globalThis.SnapGrokAuthConfig = Object.freeze({
-  "websiteUrl": "https://www.zenaian.com",
-  "serverUrl": "https://snapgrok-api.onrender.com"
-});
-})();
